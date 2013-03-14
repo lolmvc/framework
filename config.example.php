f4b6e3bb654371d38ac1c53ccfea02ab19b9afc4
Matt <matt@lolmvc.com>
2013-03-13 19:22:26 -0500

    Update config.example.php
    Updated to cover needed configuration for the App class.

---
+++ config.example.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+* Defines constants that need to be accessible throughout the
+* application
+*
+* @author Mitzip <mitzip@lolmvc.com>
+* @package MVC
+*/
+class Config {
+    /**
+    * Constructor
+    *
+    * @access public
+    * @return void
+    */
+    public function __construct() {
+        
+        /* ==============================
+         * Locale information
+         * ============================== */
+         define('TIMEZONE', 'America/Chicago');
+         define('LOCALE_CATEGORY', LC_ALL);
+         define('LOCALE_STRING', 'en_US');
+        
+        /* ==============================
+         * Debug flag (true/false)
+         * ============================== */
+        
+        define('DEBUG', false);
+    }
+}
